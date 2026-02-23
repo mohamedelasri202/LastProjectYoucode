87da30721766723cfa80bf13c00e84c803f18ee8
mohamedelasri202 <el7994995@gmail.com>
2026-02-23 00:32:15 +0000

    Add JwtUtils class for JWT token generation and validation

---
+++ EcoSimBackEnd/src/main/java/com/logistics/ecosim2/security/JwtUtils.java
@@ -0,0 +1,46 @@
+package com.logistics.ecosim2.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final int jwtExpirationMs = 86400000;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+
+            return false;
+        }
+    }
+}
