13b3d18ac12dcbcb725def645091b589706c0635
mohamedelasri202 <el7994995@gmail.com>
2026-02-23 00:34:15 +0000

    Implement UserDetailsService for user authentication by loading user details from the repository

---
+++ EcoSimBackEnd/src/main/java/com/logistics/ecosim2/security/UserDetailsService.java
@@ -0,0 +1,21 @@
+package com.logistics.ecosim2.security;
+
+import com.logistics.ecosim2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(AppUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
