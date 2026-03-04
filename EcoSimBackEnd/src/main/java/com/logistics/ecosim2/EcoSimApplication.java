8ccbb20b36b7c458327a95a85d58d9583d52e5a5
mohamedelasri202 <el7994995@gmail.com>
2026-03-04 01:51:00 +0000

    Add EcoSimApplication, RouteRequest DTO, and VehicleMapper for application setup and data mapping

---
+++ EcoSimBackEnd/src/main/java/com/logistics/ecosim2/EcoSimApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.ecosim2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoSimApplication.class, args);
+    }
+
+}
---
+++ EcoSimBackEnd/src/main/java/com/logistics/ecosim2/dtos/RouteRequest.java
@@ -0,0 +1,18 @@
+package com.logistics.ecosim2.dtos;
+
+public class RouteRequest {
+    private double startLat;
+    private double startLng;
+    private double endLat;
+    private double endLng;
+
+    // Getters and Setters
+    public double getStartLat() { return startLat; }
+    public void setStartLat(double startLat) { this.startLat = startLat; }
+    public double getStartLng() { return startLng; }
+    public void setStartLng(double startLng) { this.startLng = startLng; }
+    public double getEndLat() { return endLat; }
+    public void setEndLat(double endLat) { this.endLat = endLat; }
+    public double getEndLng() { return endLng; }
+    public void setEndLng(double endLng) { this.endLng = endLng; }
+}
