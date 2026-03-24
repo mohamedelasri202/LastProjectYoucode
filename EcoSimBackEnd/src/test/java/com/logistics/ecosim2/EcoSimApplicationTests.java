147f8aad5a416078bd135398d9c2d47c2ac12eaa
mohamedelasri202 <el7994995@gmail.com>
2026-03-24 01:42:08 +0100

    Add backend Spring Boot project

---
+++ EcoSimBackEnd/src/test/java/com/logistics/ecosim2/EcoSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.ecosim2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
